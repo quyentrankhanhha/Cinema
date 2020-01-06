6c2d54bc24062da5d83db60ec3dfb422b8582e1f
quyentrankhanhha <t9qutr00@students.oamk.fi>
2020-01-06 14:45:59 +0200

    first

---
+++ booking.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    var seats = 80;
+    var booked = [1, 2, 3];
+    var total = 0;
+    var defaultPrice = 50;
+    var price = 0;
+    var selected = '';
+    for (var i = 0; i < seats; i++) {
+        if (booked.indexOf(i + 1) == -1)
+            $('.seats').append(`<div id="S${i}" class="seat"></div>`);
+        else
+            $('.seats').append(`<div id="S${i}" class="seat booked"></div>`);
+    }
+    $('.seat:not(.booked)').click(function () {
+        if ($(this).hasClass('selected')) {
+            $(this).removeClass('selected');
+            total--;
+        }
+        else {
+            $(this).addClass('selected');
+            total++;
+            selected = selected + $(this).attr('id');
+        }
+        price = defaultPrice * total;
+        $('.total').text("Total: " + total);
+        $('.snumber').remove();
+        $('.snumber').append(`${selected},`);
+    })
+});
